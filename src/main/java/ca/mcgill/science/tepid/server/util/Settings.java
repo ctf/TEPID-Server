0b02309bd8c2ecbd33ea1abc098d556857f0f18a
AllanWang <me@allanwang.ca>
2017-09-15 18:27:57 -0400

    Remove idea files

---
+++ src/main/java/ca/mcgill/science/tepid/server/util/Settings.java
@@ -0,0 +1,35 @@
+package ca.mcgill.science.tepid.server.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Settings
+{
+
+	private static final Properties settings = new Properties();
+	
+	static
+	{
+		String configFile = System.getProperty("catalina.base") + File.separator + "conf" + File.separator + "tepid.conf";
+		try (FileInputStream in = new FileInputStream(configFile))
+		{
+			settings.load(in);
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getString(String key)
+	{
+		return settings.getProperty(key);
+	}
+	
+	public static boolean getBoolean(String key)
+	{
+		return Boolean.valueOf(settings.getProperty(key));
+	}
+	
+}
