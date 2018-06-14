9ebe552b55627de453fed4fe1b57802af687554c
Daniel Goldman <merkavabuilder@gmail.com>
2018-06-13 21:25:57 -0400

    Added Stub Unit tests for getQuota
    ref #61

--- src/test/java/ca/mcgill/science/tepid/server/rest/UserTest.kt
+++ src/test/java/ca/mcgill/science/tepid/server/rest/UserTest.kt
@@ -3,6 +3,7 @@ package ca.mcgill.science.tepid.server.rest
 import ca.mcgill.science.tepid.utils.WithLogging
 import org.junit.Test
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 class UserTest : WithLogging() {
 
@@ -13,6 +14,54 @@ class UserTest : WithLogging() {
     @Test
     fun configured() {
         assertTrue(endpoints.adminConfigured(), "Tepid is not configured")
+    }
+
+}
+
+class TestUserGetQuota : WithLogging () {
+    @Test
+    fun testGetQuotaQueriedUserNull(){
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaQueriedUserNoRole(){
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaElder(){
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaCTFer(){
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaUserIgnoreSummerSemester(){
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaUserSemesterPre2016 () {
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaUserSemester2016F () {
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaUserSemesterPost2016F () {
+        fail("Test is not implemented")
+    }
+
+    @Test
+    fun testGetQuotaUserSpanMultipleSemesters () {
+        fail("Test is not implemented")
     }
 
 }
