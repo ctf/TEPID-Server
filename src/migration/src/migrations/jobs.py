a86a01b16fc4a49f0ef622032c7285884c34c756
Daniel Goldman <merkavabuilder@gmail.com>
2018-06-27 22:58:35 -0400

    refactors for the jobs migration

--- src/migration/src/migrations/jobs.py
+++ src/migration/src/migrations/jobs.py
@@ -2,16 +2,18 @@ from migration_setup import *
 from utils import replace_null_with_value, replace_nothing_with_value
 
 
-def makeMigrationJobs00_00_00_to_00_01_00():
+def add_job_migration_views():
     ddoc.add_view('migrate_user_00_00_00_to_00_01_00', """function (doc){
   if((doc.type==="job") && (!(doc._schema) || doc._schema=="00-00-00"))
   {emit(doc._id);}
 }""")
     ddoc.save()
-    q = ddoc.get_view("migrate_user_00_00_00_to_00_01_00")
-    results = q()
 
-    for row in results:
+
+def makeMigrationJobs00_00_00_to_00_01_00():
+    to_migrate = ddoc.get_view("migrate_user_00_00_00_to_00_01_00")()
+
+    for row in to_migrate:
         doc = db[row.key]
         try:
             # type asserts
@@ -26,7 +28,16 @@ def makeMigrationJobs00_00_00_to_00_01_00():
             replace_nothing_with_value(doc, "started", -1)
             replace_nothing_with_value(doc, "processed", -1)
             replace_nothing_with_value(doc, "printed", -1)
+            # schema correction
+
+            update_schema_version(doc, "00-01-00")
+
+            doc.save()
 
 
         except TypeError as e:
             print ("migration of " + doc._id + " was aborted bue to type not matching the specification for this migration: " +  e)
+
+def update_schema_version(doc, version):
+    doc._schema = version
+
