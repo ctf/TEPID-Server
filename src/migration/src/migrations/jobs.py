631fa69c82288b31b5eeca45bcee35e209a1ccea
Daniel Goldman <merkavabuilder@gmail.com>
2018-06-27 23:53:52 -0400

    revised job migration to use Migration class

--- src/migration/src/migrations/jobs.py
+++ src/migration/src/migrations/jobs.py
@@ -1,4 +1,4 @@
-from migration import validate_migration_applicability
+from migration import validate_migration_applicability, Migration
 from migration_setup import *
 from utils import replace_null_with_value, replace_nothing_with_value, update_schema_version
 
@@ -10,29 +10,13 @@ def add_job_migration_views():
 }""")
     ddoc.save()
 
+def migrate_00_00_00_to_00_01_00(doc:str):
+    replace_nothing_with_value(doc, "started", -1)
+    replace_nothing_with_value(doc, "processed", -1)
+    replace_nothing_with_value(doc, "printed", -1)
 
-def makeMigrationJobs00_00_00_to_00_01_00():
-    to_migrate = ddoc.get_view("migrate_user_00_00_00_to_00_01_00")()
-
-    for row in to_migrate:
-        doc = db[row.key]
-        try:
-            # type asserts
-
-            validate_migration_applicability(doc, ["job"], ["00-00-00"])
-            # migration
-            replace_null_with_value(doc, "started", -1)
-            replace_null_with_value(doc, "processed", -1)
-            replace_null_with_value(doc, "printed", -1)
-            replace_nothing_with_value(doc, "started", -1)
-            replace_nothing_with_value(doc, "processed", -1)
-            replace_nothing_with_value(doc, "printed", -1)
-            # schema correction
-
-            update_schema_version(doc, "00-01-00")
-            doc.save()
-
-        except TypeError as e:
-            print ("migration of " + doc._id + " was aborted due to type not matching the specification for this migration: " +  e)
-
-
+migration_jobs_00_00_00_to_00_01_00 = Migration(
+    ["job"],
+    ["00-00-00"],
+    migrate_00_00_00_to_00_01_00,
+    "00-01-00")
