c86a7a39df348336c528f1ac6685a2e33cfa6f18
Daniel Goldman <merkavabuilder@gmail.com>
2018-06-28 00:18:28 -0400

    Stubs for applicability validator tests

---
+++ src/migration/test/test_migration.py
@@ -0,0 +1,25 @@
+"""Tests for the Migration class"""
+
+import unittest
+
+
+class Tests_Validate_Applicability(unittest.TestCase):
+    """Tests for the validators"""
+
+    ts = ["t0", "t1"]
+    vs = ["v0", "v1"]
+    # invalid items
+    invalid_t = "t2"
+    invalid_v = "v2"
+
+    def helper(self, doc_t, doc_v, types, versions):
+
+    def test_normal(self):
+
+    def test_none_for_types(self):
+
+    def test_none_for_versions(self):
+
+    def test_inapplicable_type(self):
+
+    def test_innaplicable_version(self):
