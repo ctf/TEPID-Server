05a002de593f78375cfcff1fee1c6d5f5292cde7
Daniel Goldman <merkavabuilder@gmail.com>
2018-07-01 12:55:00 -0400

    fixes to migrations to allow for different dbs
    useful for testing

--- src/migration/src/migration.py
+++ src/migration/src/migration.py
@@ -1,30 +1,32 @@
 from typing import List, Callable, Optional
 
-from migration_setup import ddoc, db
+import migration_setup
 from utils import update_schema_version
 
 
 class Migration (object):
-    def __init__(self, applicable_types: Optional[List[str]], applicable_schema_versions: Optional[List[str]], migration_function: Callable[[str], None], schema_version: str):
+    def __init__(self, applicable_types: Optional[List[str]], applicable_schema_versions: Optional[List[str]], migration_function: Callable[[str], None], schema_version: str, db=migration_setup.db, ddoc=migration_setup.ddoc):
         self.applicable_types = applicable_types
         self.applicable_schema_versions = applicable_schema_versions
         self.migration_function = migration_function
         self.schema_version = schema_version
+        self.db = db
+        self.ddoc = ddoc
 
     def make(self, doc: str):
         self.migration_function(doc)
         update_schema_version(doc, self.schema_version)
 
     def apply_on_view(self, view):
-        to_migrate = ddoc.get_view(view)()
+        to_migrate = self.ddoc.get_view(view)()['rows']
         for row in to_migrate:
-            doc = db[row.key]
+            doc = self.db[row['key']]
             try:
                 validate_migration_applicability(doc, self.applicable_types, self.applicable_schema_versions)
                 self.make(doc)
                 doc.save()
             except TypeError as e:
-                print ("migration of " + doc._id + " was aborted due to type not matching the specification for this migration: " +  e)
+                print("migration of " + doc['_id'] + " was aborted due to type not matching the specification for this migration: " + str(e))
 
 
 def validate_type_applicable(doc_type: str, types: List[str]):
