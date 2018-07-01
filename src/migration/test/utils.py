a862ce7aa8dc5c4cf8e7399dc914e395458a3f90
Daniel Goldman <merkavabuilder@gmail.com>
2018-07-01 12:51:38 -0400

    Added function to compare content of Documents
    ignores couchdb metadata like _id and _rev

--- src/migration/test/utils.py
+++ src/migration/test/utils.py
@@ -5,9 +5,17 @@ from cloudant.document import Document
 
 project_root = os.environ['PYTHONPATH'].split(os.pathsep)[0]
 
+
 def document_from_json_file(db, file_path):
     with open(file_path) as blob:
         json_obj = json.loads(blob.read())
         doc = Document(db, json_obj["_id"])
         doc.update(json_obj)
         return doc
+
+
+def compare_document_content(this, that):
+    meta = ["_id", "_rev"]
+    this_filtered = dict((k, v) for k, v in this.items() if k not in meta)
+    that_filtered = dict((k, v) for k, v in that.items() if k not in meta)
+    return this_filtered == that_filtered
