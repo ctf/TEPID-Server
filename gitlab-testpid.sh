3970eeb524fa8ae073f84d3872b0004fa6df07b0
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2018-09-08 15:21:40 -0400

    Update gitlab-testpid.sh: dockerised tomcat autostarts new WAR no need to testart

--- gitlab-testpid.sh
+++ gitlab-testpid.sh
@@ -10,22 +10,13 @@ printf "Starting testpid script...\n"
 echo "$CI_PROJECT_DIR"
 echo "$CI_COMMIT_SHA"
 
-TO_DELETE=false
-
 chmod +x ./gradlew
 ./gradlew clean build test war
-
-if [ "$TO_DELETE" = true ]; then
-    printf "Remove priv properties\n"
-    rm priv.properties
-fi
 
 if [ -e build/libs/tepid.war ]; then
     printf "Moving war file\n"
 	mv -f build/libs/tepid.war /var/lib/tomcat8/webapps/tepid.war
-#	rm -rf /var/lib/tomcat8/webapps/tepid
 	printf "Restarting tepid\n"
-	sudo systemctl restart tomcat8.service
 else
 	printf "Could not find tepid.war in build output\n"
 	exit 1
