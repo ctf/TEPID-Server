0c7b41e5532e1bb7d35935b64c2d126f90ec5275
Allan Wang <me@allanwang.ca>
2018-09-28 20:05:51 -0400

    Add remaining db layers

--- src/main/java/ca/mcgill/science/tepid/server/db/DbLayer.kt
+++ src/main/java/ca/mcgill/science/tepid/server/db/DbLayer.kt
@@ -1,10 +1,11 @@
 package ca.mcgill.science.tepid.server.db
 
-import ca.mcgill.science.tepid.models.data.FullDestination
+import ca.mcgill.science.tepid.models.data.*
 import javax.ws.rs.core.Response
 
 // TODO deleteDestination should return Response instead of String
 // TODO, all outputs returning response should likely return models that can then be wrapped inside a response
+// TODO outputs should be nullable
 
 /**
  * Ids are unique string keys in dbs
@@ -12,10 +13,30 @@ import javax.ws.rs.core.Response
 typealias Id = String
 
 /**
- * Abstraction layer for all db transactions
- * Inputs and outputs must be pure JVM models that are separated from db models
+ * String representing a:
+ * - short user
+ * - long user
+ * - student id
  */
-interface DbLayer : DbDestinationLayer
+typealias Sam = String
+
+enum class Order {
+    ASCENDING, DESCENDING
+}
+
+/**
+ * Abstraction layer for all db transactions.
+ * Inputs and outputs must be pure JVM models that are separated from db models.
+ * Note that layers are categorized based on particular models to help with delegation patterns.
+ * Each method in each of the included interfaces should still be unique with respect to all other methods.
+ */
+interface DbLayer :
+        DbDestinationLayer,
+        DbJobLayer,
+        DbQueueLayer,
+        DbMarqueeLayer,
+        DbSessionLayer,
+        DbUserLayer
 
 interface DbDestinationLayer {
 
@@ -30,3 +51,71 @@ interface DbDestinationLayer {
      */
     fun deleteDestination(id: Id): String
 }
+
+interface DbJobLayer {
+
+    fun getJob(id: Id): PrintJob
+
+    /**
+     * Get jobs by [queue]
+     * If [maxAge] > 0, then only jobs created before (now - [maxAge])ms will be included
+     * If [limit] > 0, then only [limit] jobs will be provided
+     */
+    fun getJobsByQueue(queue: String,
+                       maxAge: Long = -1,
+                       sortOrder: Order = Order.DESCENDING,
+                       limit: Int = -1): List<PrintJob>
+
+    fun getJobsByUser(sam: Sam, sortOrder: Order = Order.DESCENDING): List<PrintJob>
+
+    /**
+     * Updates the job, and returns the new job if successful
+     */
+    fun updateJob(id: Id, updater: PrintJob.() -> Unit): PrintJob?
+
+    fun postJob(job: PrintJob): Response
+
+    fun getJobChanges(id: Id): ChangeDelta
+
+    fun getJobFile(id: Id, file: String): Response
+
+}
+
+interface DbQueueLayer {
+
+    fun getQueues(): List<PrintQueue>
+
+    fun putQueues(queues: Collection<PrintQueue>): Response
+
+}
+
+interface DbMarqueeLayer {
+
+    fun getMarquees(): List<MarqueeData>
+
+}
+
+interface DbSessionLayer {
+
+    fun putSession(session: FullSession): Response
+
+    fun getSessionOrNull(id: Id): FullSession?
+
+    fun deleteSession(id: Id): String
+
+}
+
+interface DbUserLayer {
+
+    fun putUser(user: FullUser): Response
+
+    fun getUserOrNull(sam: Sam): FullUser?
+
+    fun isAdminConfigured(): Boolean
+
+    // TODO delete? Or at least use shortUser from user
+    fun putAdmin(shortUser: String, user: FullUser): Response
+
+    fun getTotalPrintedCount(shortUser: String): Int
+}
+
