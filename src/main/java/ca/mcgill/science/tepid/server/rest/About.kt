55a055723d667e26e64b20eb632e79fb96aabc40
Daniel Goldman <merkavabuilder@gmail.com>
2019-01-19 14:22:33 -0500

    Add about endpoint for swagger-ui.jsp

--- src/main/java/ca/mcgill/science/tepid/server/rest/About.kt
+++ src/main/java/ca/mcgill/science/tepid/server/rest/About.kt
@@ -2,16 +2,17 @@ package ca.mcgill.science.tepid.server.rest
 
 import ca.mcgill.science.tepid.models.data.About
 import ca.mcgill.science.tepid.server.server.Config
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
 
 /**
- * Created by Allan Wang on 2017-11-19.
- *
- * Public endpoint to get immediate information about the current build
- * This is also unaffected by CouchDb, and will help with debugging
+ * Endpoint to get information about the current build
+ * Useful for debugging
  */
 @Path("/about")
 class About {
@@ -19,5 +20,13 @@ class About {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     fun getAbout(): About = Config.PUBLIC
+
+    @GET
+    @Path("/api")
+
+    fun api(@Context ctxRequest: HttpServletRequest, @Context ctxResponse: HttpServletResponse) {
+
+            ctxRequest.getRequestDispatcher("/swagger-ui.jsp").forward(ctxRequest,ctxResponse)
+    }
 
 }
