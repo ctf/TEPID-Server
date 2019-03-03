17a69ecbff722b2535f3ba3f57a47e09d28d6a06
Daniel Goldman <merkavabuilder@gmail.com>
2019-03-02 23:16:18 -0500

    Add implementation for getDestinations
    Oh also HibernateDestinationLayer became a thing

--- src/main/java/ca/mcgill/science/tepid/server/db/PSQL.kt
+++ src/main/java/ca/mcgill/science/tepid/server/db/PSQL.kt
@@ -1,7 +1,16 @@
 package ca.mcgill.science.tepid.server.db
 
+import ca.mcgill.science.tepid.models.data.FullDestination
 import ca.mcgill.science.tepid.models.data.MarqueeData
+import ca.mcgill.science.tepid.server.util.mapper
+import javax.ws.rs.core.Response
 
+
+class HibernateDestinationLayer(val hc : HibernateCrud) : DbDestinationLayer{
+    override fun getDestinations(): List<FullDestination> {
+        return hc.readAll(FullDestination::class.java)
+    }
+}
 
 class HibernateMarqueeLayer(val hc : HibernateCrud) : DbMarqueeLayer {
 
