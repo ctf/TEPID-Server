87fdc0cfdfe1dd02ca9c1daa9fbd028616eb2768
Daniel Goldman <merkavabuilder@gmail.com>
2019-03-23 02:28:31 -0400

    Use the test rig from commons

---
+++ src/integration-test/java/ca/mcgill/science/tepid/server/rest/ITBase.kt
@@ -0,0 +1,8 @@
+package ca.mcgill.science.tepid.server.rest
+
+import ca.mcgill.science.tepid.test.TestUtilsContract
+import ca.mcgill.science.tepid.test.TestUtilsDelegate
+
+open class ITBase: TestUtilsContract by TestUtilsDelegate(){
+
+}
--- src/integration-test/java/ca/mcgill/science/tepid/server/rest/JobIT.kt
+++ src/integration-test/java/ca/mcgill/science/tepid/server/rest/JobIT.kt
@@ -1,70 +1,28 @@
 package ca.mcgill.science.tepid.server.rest
 
-import ca.mcgill.science.tepid.models.data.FullUser
 import ca.mcgill.science.tepid.models.data.PrintJob
-import ca.mcgill.science.tepid.models.data.SessionRequest
-import ca.mcgill.science.tepid.server.auth.SessionManager
 import ca.mcgill.science.tepid.server.db.DB
-import ca.mcgill.science.tepid.server.server.Config
-import ca.mcgill.science.tepid.server.util.getSession
+import ca.mcgill.science.tepid.test.get
+import ca.mcgill.science.tepid.utils.Loggable
 import ca.mcgill.science.tepid.utils.WithLogging
-import io.mockk.MockKAnnotations
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.unmockkAll
 import org.junit.After
-import org.junit.Assume
 import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import javax.ws.rs.container.ContainerRequestContext
-import kotlin.test.fail
 
 @ExtendWith(MockKExtension::class)
-class JobTest : WithLogging() {
+class JobTest : ITBase(), Loggable by WithLogging() {
 
-    val endpoints: Jobs by lazy {
-        Jobs()
-    }
-    val Sessions by lazy {
-        Sessions()
-    }
-
-    lateinit var testUser: FullUser
     lateinit var testJob: PrintJob
-
-    @MockK
-    lateinit var mockUserCtx: ContainerRequestContext
 
     @Before
     fun initTest() {
-
-        MockKAnnotations.init(this)
-
-        Config.TEST_USER;
-
-        testUser = SessionManager.queryUser(Config.TEST_USER, null) ?: fail("Test User with SAM ${Config.TEST_USER} not found")
-
-        Assume.assumeTrue(Config.LDAP_ENABLED)
-        Assume.assumeTrue(Config.TEST_USER.isNotEmpty())
-        Assume.assumeTrue(Config.TEST_PASSWORD.isNotEmpty())
-        println("Running ldap tests with test user")
-
-        val testSession = Sessions.startSession(SessionRequest(Config.TEST_USER, Config.TEST_PASSWORD, false, false))
-        val testFullSession = DB.getSessionOrNull(testSession._id!!) ?: fail("session could not be created. {" +
-                "  \"shortUser\": {$Config.TEST_USER}," +
-                "  \"sessionId\": {$testSession._id}" +
-                "}")
-
-        every {
-            mockUserCtx.getSession()
-        } returns testFullSession
-
         testJob = PrintJob(
-                name= "TEST",
+                name= "Server Test ${System.currentTimeMillis()}",
                 queueName = DB.getQueues().first().name,
-                userIdentification = testUser.shortUser
+                userIdentification = testUser
         )
     }
 
@@ -75,7 +33,8 @@ class JobTest : WithLogging() {
 
     @Test
     fun testNewJob() {
-        endpoints.newJob(testJob, mockUserCtx)
+        testApi.createNewJob(PrintJob(name="Server Test ${System.currentTimeMillis()}")).get()
+
     }
 
 }
