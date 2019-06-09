35a854c1ed9d2ffd27f1540f28ec776c9a6bc159
Daniel Goldman <merkavabuilder@gmail.com>
2019-06-09 17:53:29 -0400

    build ITBase from config

--- src/integration-test/java/ca/mcgill/science/tepid/server/rest/ITBase.kt
+++ src/integration-test/java/ca/mcgill/science/tepid/server/rest/ITBase.kt
@@ -1,10 +1,16 @@
 package ca.mcgill.science.tepid.server.rest
 
-import ca.mcgill.science.tepid.test.TestUtilsContract
+import ca.mcgill.science.tepid.server.server.Config
 import ca.mcgill.science.tepid.test.TestUtilsDelegate
+import ca.mcgill.science.tepid.utils.PropsLDAPTestUser
+import ca.mcgill.science.tepid.utils.PropsURL
 
 
-open class ITBase { // so open class ITBase : TestUtilsContract by TestUtilsDelegate() gives a thing where it prepends https:// to the URL, which already has that
-    object server : TestUtilsContract by TestUtilsDelegate(){ // this works though
-    }
+open class ITBase(val cfg: Config = Config) {
+    val server = TestUtilsDelegate(
+            PropsLDAPTestUser.TEST_USER,
+            PropsLDAPTestUser.TEST_PASSWORD,
+            PropsURL.SERVER_URL_PRODUCTION!!,
+            PropsURL.TESTING!!.toBoolean()
+    )
 }
