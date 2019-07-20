ad0a7fdbdb8fc3b4c61509c440b079db8462caa1
Daniel Goldman <merkavabuilder@gmail.com>
2019-07-20 15:00:17 -0400

    properly handle response for print in reprint

--- src/integration-test/java/ca/mcgill/science/tepid/server/rest/JobIT.kt
+++ src/integration-test/java/ca/mcgill/science/tepid/server/rest/JobIT.kt
@@ -7,6 +7,7 @@ import ca.mcgill.science.tepid.models.data.FullDestination
 import ca.mcgill.science.tepid.models.data.PrintJob
 import ca.mcgill.science.tepid.models.data.PrintQueue
 import ca.mcgill.science.tepid.server.server.Config
+import ca.mcgill.science.tepid.test.TestUtils
 import ca.mcgill.science.tepid.utils.Loggable
 import ca.mcgill.science.tepid.utils.WithLogging
 import org.junit.jupiter.api.BeforeEach
@@ -14,9 +15,7 @@ import org.junit.jupiter.api.Test
 import java.io.File
 import java.io.FileInputStream
 import java.util.concurrent.TimeUnit
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
-import kotlin.test.fail
+import kotlin.test.*
 
 class JobTest : ITBase(), Loggable by WithLogging() {
 
@@ -31,7 +30,7 @@ class JobTest : ITBase(), Loggable by WithLogging() {
 
         server.testApi.putDestinations(mapOf("d0" to FullDestination(name = "d0", up = true), "d1" to FullDestination(name = "d1", up = true))).executeDirect()
 
-        val q =  PrintQueue(loadBalancer = "fiftyfifty", name = "q0", destinations = listOf("d0", "d1"))
+        val q = PrintQueue(loadBalancer = "fiftyfifty", name = "q0", destinations = listOf("d0", "d1"))
         q._id = "q0"
         server.testApi.putQueues(listOf(q)).executeDirect()
 
@@ -83,12 +82,12 @@ class JobTest : ITBase(), Loggable by WithLogging() {
         assertTrue(response.ok)
 
         // turn off original destination
-        TimeUnit.MILLISECONDS.sleep(500)
+        TimeUnit.MILLISECONDS.sleep(1000)
 
         val printedJob = server.testApi.getJob(jobId).executeDirect()
                 ?: fail("did not retrieve printed job after print")
 
-        val setStatusResponse = server.testApi.setPrinterStatus(printedJob.destination
+        val setStatusResponse = TestUtils.testApi.setPrinterStatus(printedJob.destination
                 ?: fail("printed job did not have destination"), DestinationTicket(up = false, reason = "reprint test, put me up")).execute()
 
         if (setStatusResponse?.body()?.contains("down") != true) {
@@ -96,13 +95,19 @@ class JobTest : ITBase(), Loggable by WithLogging() {
         }
 
         // reprint
-        val reprintResponse = server.testApi.reprintJob(jobId).executeDirect()
+        val reprintResponse = server.testApi.reprintJob(jobId).execute().body()
                 ?: fail("did not retrieve response after reprint")
 
+
         assertFalse(reprintResponse.contains("Failed"))
-        assertFalse(reprintResponse.contains(jobId))
+
+        val foundIds = server.GUID_REGEX.findAll(reprintResponse).map { f -> f.value }.toList()
+        assertEquals(2, foundIds.size)
+        assertEquals(jobId, foundIds[0])
+        assertNotEquals(jobId, foundIds[1])
 
 
     }
+
 
 }
