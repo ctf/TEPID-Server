a7f6dc20df042fa1ac405c565cf12bdad2777d4e
Daniel Goldman <merkavabuilder@gmail.com>
2019-08-30 18:19:12 -0400

    instantiate couchdb client is now a function

--- src/migration/src/migration_setup.py
+++ src/migration/src/migration_setup.py
@@ -24,17 +24,18 @@ def parseConfig(filePath):
                 k, v = line.split("=", 1)
                 keys[k.strip()] = v.strip()
 
-        return DbConfig(keys["COUCHDB_USERNAME"], keys["COUCHDB_PASSWORD"], keys["COUCHDB_URL"])
+        return DbConfig(keys["USERNAME"], keys["PASSWORD"], keys["URL"])
 
-print (os.getcwd())
-Config = parseConfig(conf_dir + "DB.properties")
-client = CouchDB(user=Config.dbUsername,
-                 auth_token=Config.dbPassword,
-                 url=Config.dbUrlBase,
-                 admin_party=False,
-                 auto_renew=True)
-client.connect()
-session = client.session()
-db = client[Config.dbUrlDb]
-ddoc = DesignDocument(db, 'migrate')
-ddoc.create()
+def loadCouchDb(file):
+    Config = parseConfig(file)
+    client = CouchDB(user=Config.dbUsername,
+                     auth_token=Config.dbPassword,
+                     url=Config.dbUrlBase,
+                     admin_party=False,
+                     auto_renew=True)
+    client.connect()
+    session = client.session()
+    db = client[Config.dbUrlDb]
+    ddoc = DesignDocument(db, 'migrate')
+    ddoc.create()
+    return client, db, session, ddoc
