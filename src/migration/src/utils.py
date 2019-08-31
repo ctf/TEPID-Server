086892e6be3711e804c12a87e6dfdcd7d3d91399
Daniel Goldman <merkavabuilder@gmail.com>
2019-08-31 01:20:24 -0400

    add utility method for SQL nullables

--- src/migration/src/utils.py
+++ src/migration/src/utils.py
@@ -11,3 +11,9 @@ def replace_nothing_with_value(dict, property, value):
 def update_schema_version(doc, version):
     if version is not None:
         doc["schema"] = version
+
+def getQuotedOrNull(doc, key):
+	v = doc.get(key, None)
+	if v is None:
+		return 'NULL'
+	return f"'{v}'"
