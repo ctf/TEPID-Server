96d2c55f3ad1736b58f40c0b946857a2a463b438
Daniel Goldman <merkavabuilder@gmail.com>
2019-09-04 00:16:58 -0400

    delete unused method

--- src/main/java/ca/mcgill/science/tepid/server/auth/LdapHelper.kt
+++ src/main/java/ca/mcgill/science/tepid/server/auth/LdapHelper.kt
@@ -7,24 +7,10 @@ import javax.naming.directory.Attributes
  * Some helper functions for ldap
  */
 interface LdapHelperContract {
-    fun Attributes.toList(): List<Attribute>
     fun Attribute.toList(): List<String>
 }
 
 class LdapHelperDelegate : LdapHelperContract {
-    /**
-     * Convert attributes to attribute list
-     */
-    override fun Attributes.toList(): List<Attribute> {
-        val ids = iDs
-        val data = mutableListOf<Attribute>()
-        while (ids.hasMore()) {
-            val id = ids.next()
-            data.add(get(id))
-        }
-        ids.close()
-        return (data)
-    }
 
     /**
      * Convert attribute to string list
