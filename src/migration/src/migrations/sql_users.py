9c0c33094ee307e0d89ae637e8d42965ff5316b4
Daniel Goldman <merkavabuilder@gmail.com>
2019-09-07 00:17:32 -0400

    fix user migration

--- src/migration/src/migrations/sql_users.py
+++ src/migration/src/migrations/sql_users.py
@@ -5,7 +5,7 @@ from utils import getQuotedOrNull
 
 def add_sql_users_migration_views(ddoc):
 	ddoc.add_view('migrate_user_00_01_00_to_sql', """function (doc){
-	if ((doc.type==="user") && (doc.schema=="00-01-00")) {emit(doc._id);}
+	if ((doc.type==="user") && (doc.role)) {emit(doc._id);}
 	 }"""
 				  )
 	ddoc.save()
@@ -13,17 +13,18 @@ def add_sql_users_migration_views(ddoc):
 
 def migrate_users_to_sql(doc: dict):
 	o = doc
-
 	if doc.get('role', None) is None:
+		print("no role, not migrating")
 		return
-
-	sql = f"""insert into fulluser(_id, activesince, authtype, colorprinting, displayname, email, faculty, givenname, jobexpiration, lastname, longuser, middlename, nick, password, preferredname, realname, role, salutation, shortuser, studentid) values ('{o['_id']}','{o.get('activeSince', -1)}', {getQuotedOrNull(doc, 'authType')}, '{o['colorPrinting']}', '{o['displayName']}', '{o['email']}', {getQuotedOrNull(doc, 'faculty')}, '{o['givenName']}', '{o['jobExpiration']}', '{o['lastName']}', '{o['longUser']}', {getQuotedOrNull(doc, 'middleName')}, {getQuotedOrNull(doc, 'nick')}, {getQuotedOrNull(doc, 'password')},NULL, {getQuotedOrNull(doc, 'realName')}, {getQuotedOrNull(doc, 'role')}, {getQuotedOrNull(doc, 'salutation')}, {getQuotedOrNull(doc, 'shortUser')}, '{o.get('studentId', -1)}') ON CONFLICT DO NOTHING;"""
-
-	print(">>>>>>>", sql)
-
+	sql = f"""insert into fulluser(_id, activesince, authtype, colorprinting, displayname, email, faculty, givenname, jobexpiration, lastname, longuser, middlename, nick, password, preferredname, realname, role, salutation, shortuser, studentid) values ({getQuotedOrNull(doc,'_id')},'{doc.get('activeSince', -1)}', {getQuotedOrNull(doc, 'authType')}, {getQuotedOrNull(doc, 'colorPrinting')}, {getQuotedOrNull(doc,'displayName')}, {getQuotedOrNull(doc,'email')}, {getQuotedOrNull(doc, 'faculty')}, {getQuotedOrNull(doc, 'givenName')}, {getQuotedOrNull(doc, 'jobExpiration')}, {getQuotedOrNull(doc,'lastName')}, {getQuotedOrNull(doc, 'longUser')}, {getQuotedOrNull(doc, 'middleName')}, {getQuotedOrNull(doc, 'nick')}, {getQuotedOrNull(doc, 'password')},NULL, {getQuotedOrNull(doc, 'realName')}, {getQuotedOrNull(doc, 'role')}, {getQuotedOrNull(doc, 'salutation')}, {getQuotedOrNull(doc, 'shortUser')}, '{o.get('studentId', -1)}') ON CONFLICT DO NOTHING;"""
 	cur = con.cursor()
-	cur.execute(sql)
-	con.commit()
+	try:
+		cur.execute(sql)
+		con.commit()
+	except Exception as e:
+		con.rollback()
+		cur.close()
+		raise e
 	cur.close()
 
 
@@ -31,6 +32,6 @@ migration_users_to_sql = Migration(
 	['user'],
 	None,
 	migrate_users_to_sql,
-	['00-01-00'],
+	['00-01-01'],
 	db,
 	ddoc)
