9c53b26837baf520ac513380479a685d0e824fbb
Daniel Goldman <merkavabuilder@gmail.com>
2019-10-24 23:24:55 -0400

    add migration for adding attribute of fulluser for semester

---
+++ src/migration/sql/1.sql
@@ -0,0 +1,19 @@
+--
+-- Name: fulluser_semesters; Type: TABLE; Schema: public; Owner: tepid
+--
+
+CREATE TABLE public.fulluser_semesters (
+                                           fulluser__id character varying(36) NOT NULL,
+                                           season integer,
+                                           year integer NOT NULL
+);
+
+
+ALTER TABLE public.fulluser_semesters OWNER TO tepid;
+
+--
+-- Name: fulluser_semesters fk7boa81tl3qqsoh85n6raknp63; Type: FK CONSTRAINT; Schema: public; Owner: tepid
+--
+
+ALTER TABLE ONLY public.fulluser_semesters
+    ADD CONSTRAINT fk7boa81tl3qqsoh85n6raknp63 FOREIGN KEY (fulluser__id) REFERENCES public.fulluser(_id);
