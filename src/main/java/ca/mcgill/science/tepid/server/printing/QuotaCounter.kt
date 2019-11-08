166a81d755c7847b31a1607ab93325d7b4d40281
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2019-11-08 00:02:04 -0500

    add method to get all eligible users

--- src/main/java/ca/mcgill/science/tepid/server/printing/QuotaCounter.kt
+++ src/main/java/ca/mcgill/science/tepid/server/printing/QuotaCounter.kt
@@ -2,9 +2,12 @@ package ca.mcgill.science.tepid.server.printing
 
 import ca.mcgill.science.tepid.models.data.FullUser
 import ca.mcgill.science.tepid.models.data.Semester
+import ca.mcgill.science.tepid.server.auth.LdapConnector
 import ca.mcgill.science.tepid.server.server.Config
 
 object QuotaCounter {
+
+    private val ldapConnector = LdapConnector()
 
     fun addSemester(user: FullUser, semester: Semester = Semester.current) {
         user.semesters = user.semesters.plus(semester)
@@ -16,5 +19,10 @@ object QuotaCounter {
      */
     fun hasCurrentSemesterEligible(user: FullUser, registeredSemesters: Set<Semester>): Boolean {
         return (Config.USERS_GROUP.any(user.groups::contains) && registeredSemesters.contains(Semester.current))
+    }
+
+    fun getAllCurrentlyEligible(): Set<FullUser>? {
+        val filter = "(|${Config.USERS_GROUP.map { "(memberOf:1.2.840.113556.1.4.1941:=cn=${it.name},${Config.GROUPS_LOCATION})" }.joinToString()})"
+        return ldapConnector.executeSearch(filter, Long.MAX_VALUE)
     }
 }
