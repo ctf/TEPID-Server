2305d509ae45a99fc8fc849250bb62d12b38de0f
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2019-11-09 23:20:02 -0500

    add paged search for big searches

--- src/main/java/ca/mcgill/science/tepid/server/auth/LdapConnector.kt
+++ src/main/java/ca/mcgill/science/tepid/server/auth/LdapConnector.kt
@@ -11,8 +11,11 @@ import javax.naming.Context
 import javax.naming.NamingEnumeration
 import javax.naming.directory.SearchControls
 import javax.naming.directory.SearchResult
+import javax.naming.ldap.Control
 import javax.naming.ldap.InitialLdapContext
 import javax.naming.ldap.LdapContext
+import javax.naming.ldap.PagedResultsControl
+import javax.naming.ldap.PagedResultsResponseControl
 
 class LdapConnector(val timeout: Int? = 5000) : Logging {
 
@@ -28,7 +31,13 @@ class LdapConnector(val timeout: Int? = 5000) : Logging {
      */
 
     fun bindLdap(shortUser: ShortUser, password: String): LdapContext? {
-        logger.trace { logMessage("attempting bind to LDAP", "PROVIDER_URL" to Config.PROVIDER_URL, "SECURITY_PRINCIPAL" to Config.SECURITY_PRINCIPAL_PREFIX + shortUser) }
+        logger.trace {
+            logMessage(
+                "attempting bind to LDAP",
+                "PROVIDER_URL" to Config.PROVIDER_URL,
+                "SECURITY_PRINCIPAL" to Config.SECURITY_PRINCIPAL_PREFIX + shortUser
+            )
+        }
         try {
             val auth = createAuthMap(shortUser, password)
             return InitialLdapContext(auth, null)
@@ -40,35 +49,75 @@ class LdapConnector(val timeout: Int? = 5000) : Logging {
     }
 
     fun executeSearch(searchFilter: String, limit: Long = 10): Set<FullUser>? {
-        var results: NamingEnumeration<SearchResult>? = null
+        var results: List<SearchResult>? = null
         val ctx = bindLdapWithResource() ?: return null
         try {
             val searchControls = SearchControls()
             searchControls.searchScope = SearchControls.SUBTREE_SCOPE
             searchControls.countLimit = limit
 
-            results = ctx.search(Config.LDAP_SEARCH_BASE, searchFilter, searchControls)
+            if (limit > 1000 || limit <= 0) {
+                results = executePagedSearch(
+                    ctx,
+                    { c: LdapContext -> c.search(Config.LDAP_SEARCH_BASE, searchFilter, searchControls) })
+            } else {
+                results = ctx.search(Config.LDAP_SEARCH_BASE, searchFilter, searchControls).toList()
+            }
 
-            val users = results.toList().map { LdapHelper.AttributesToUser(it.attributes, ctx) }.toSet()
-            return users
+            return results.map { LdapHelper.AttributesToUser(it.attributes, ctx) }.toSet()
         } finally {
-            results?.close()
             ctx.close()
         }
     }
 
-    private companion object : Logging {
-        /**
-         * Defines the environment necessary for [InitialLdapContext]
-         */
-        private fun createAuthMap(user: ShortUser, password: String) = Hashtable<String, String>().apply {
-            put(Context.SECURITY_AUTHENTICATION, "simple")
-            put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
-            put(Context.PROVIDER_URL, Config.PROVIDER_URL)
-            put(Context.SECURITY_PRINCIPAL, Config.SECURITY_PRINCIPAL_PREFIX + user)
-            put(Context.SECURITY_CREDENTIALS, password)
+    protected fun executePagedSearch(
+        ctx: LdapContext,
+        todo: (LdapContext) -> NamingEnumeration<SearchResult>,
+        pageSize: Int = 1000
+    ): List<SearchResult> {
+
+        var cookie: kotlin.ByteArray? = null
+        ctx.requestControls = arrayOf<Control>(
+            PagedResultsControl(
+                pageSize,
+                Control.NONCRITICAL
+            )
+        )
+        var out: List<SearchResult> = listOf()
+
+        do {
+            val results = todo(ctx)
+            out = out.plus(results.toList())
+
+            val controls = ctx.responseControls
+
+            if (controls != null) {
+                for (c in controls) {
+                    if (c is PagedResultsResponseControl) {
+                        cookie = c.cookie
+                    }
+                }
+            }
+            ctx.requestControls = arrayOf<Control>(
+                PagedResultsControl(
+                    pageSize, cookie, Control.CRITICAL
+                )
+            )
+        } while (cookie != null)
+
+        return out
+    }
+
+    /**
+     * Defines the environment necessary for [InitialLdapContext]
+     */
+    private fun createAuthMap(user: ShortUser, password: String) = Hashtable<String, String>().apply {
+        put(Context.SECURITY_AUTHENTICATION, "simple")
+        put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
+        put(Context.PROVIDER_URL, Config.PROVIDER_URL)
+        put(Context.SECURITY_PRINCIPAL, Config.SECURITY_PRINCIPAL_PREFIX + user)
+        put(Context.SECURITY_CREDENTIALS, password)
         put("com.sun.jndi.ldap.read.timeout", timeout.toString())
-            put("com.sun.jndi.ldap.connect.timeout", "500")
-        }
+        put("com.sun.jndi.ldap.connect.timeout", "500")
     }
 }
