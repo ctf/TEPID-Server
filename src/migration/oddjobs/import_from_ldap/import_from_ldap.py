c3d77c19390fff972b2f7730b81a71c4779efad0
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2020-02-20 01:20:42 -0500

    build filter for groups

--- src/migration/oddjobs/import_from_ldap/import_from_ldap.py
+++ src/migration/oddjobs/import_from_ldap/import_from_ldap.py
@@ -10,6 +10,17 @@ def get_eligible_users_from_ldap(props) -> List[str]:
 	l.protocol_version = ldap.VERSION3
 	l.set_option(ldap.OPT_REFERRALS, 0)
 
+	unparsed_groups = map(lambda s: s.split('+'), [
+		props['LDAPGroups']['QUOTA_GROUPS'],
+		props['LDAPGroups']['CTFERS_GROUPS'],
+		props['LDAPGroups']['ELDERS_GROUPS'],
+		])
+	quota_groups = [item for sublist in unparsed_groups for item in sublist]
+
+	quota_groups_filter = ''.join(
+		map(lambda g: f"(memberOf:1.2.840.113556.1.4.1941:=cn={g},{props['LDAPGroups']['GROUPS_LOCATION']})",
+			quota_groups))
+
 	l.bind_s(
 		props['LDAP']['SECURITY_PRINCIPAL_PREFIX'] + props['LDAPResource']['LDAP_RESOURCE_USER'],
 		props['LDAPResource']['LDAP_RESOURCE_CREDENTIALS']
